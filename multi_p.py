39adc2dd494c421d1f2df6419d52a1b63dfda7fc
Igor Shakirov <sim2egor@mail.ru>
2022-07-04 12:11:09 +0300

    222

---
+++ multi_p.py
@@ -0,0 +1,32 @@
+import multiprocessing
+import time
+
+
+def worker(num, arr):
+    num.value = 3.1415927
+    for i in range(len(arr)):
+        arr[i] = -arr[i]
+    for i in range(1000) :
+        num.value = i
+        time.sleep(0.0001)
+
+if __name__ == '__main__':
+    num = multiprocessing.Value('d', 0.0)
+    arr = multiprocessing.Array('i', range(10))
+
+    p = multiprocessing.Process(target=worker, name="Pr1", args=(num, arr))
+    p.start()
+    for i in range(100):
+        if i == 30:
+            p.terminate()
+        time.sleep(0.00015)
+        print(" Живой ? {} {}".format(p.is_alive(),i))
+
+    print(arr[:])
+    print(p.is_alive())
+    p.join()
+    print("Global {}".format(num.value))
+    p.close()
+    p = multiprocessing.Process(target=worker, name="Pr1", args=(num, arr))
+    p.start()
+    p.join()
