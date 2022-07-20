688c882052dd1d2587beecb60c19cc6658c01d4a
Igor Shakirov <sim2egor@mail.ru>
2022-07-20 14:08:33 +0300

    test multu

--- multi_p.py
+++ multi_p.py
@@ -9,6 +9,12 @@ def worker(num, arr):
     for i in range(1000) :
         num.value = i
         time.sleep(0.0001)
+    print(arr[:])
+    print(">>>>>>>>>>>>>>>>>>>>>")
+    while arr[0]==0:
+        print("hop! %s",num.value)
+        num.value = num.value+1
+        time.sleep(1)
 
 if __name__ == '__main__':
     num = multiprocessing.Value('d', 0.0)
@@ -16,15 +22,20 @@ if __name__ == '__main__':
 
     p = multiprocessing.Process(target=worker, name="Pr1", args=(num, arr))
     p.start()
-    for i in range(100):
-        if i == 30:
-            p.terminate()
-        time.sleep(0.00015)
-        print(" Живой ? {} {}".format(p.is_alive(),i))
+    # for i in range(100):
+    #     if i == 30:
+    #         p.terminate()
+    #     time.sleep(0.00015)
+    #     print(" Живой ? {} {}".format(p.is_alive(),i))
 
     print(arr[:])
     print(p.is_alive())
+    time.sleep(5)
+    print("first step")
+    print(arr[:])
+    arr[0]=1
     p.join()
+    print("---------------------------------------")
     print("Global {}".format(num.value))
     p.close()
     p = multiprocessing.Process(target=worker, name="Pr1", args=(num, arr))
